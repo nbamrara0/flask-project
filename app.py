672c9c03d08f02a3fe29c40eece2c17252eca87c
Naveen <nbamrara0@gmail.com>
2026-05-21 11:57:13 +0530

    update index.html

--- app.py
+++ app.py
@@ -113,3 +113,5 @@ def predict():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+# deploy url https://flask-project-1-foev.onrender.com
