3400400e2ac4c7d07e7d7b68589b52569041eeb0
Naveen <nbamrara0@gmail.com>
2026-05-23 11:39:19 +0530

    fix port for render

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ database_url = os.getenv("DATABASE_URL")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+    "connect_args": {"sslmode": "require"}
+}
 
 db = SQLAlchemy(app)
 # ---------------- LOAD MODEL ----------------
@@ -73,7 +76,7 @@ def predict():
     )
 
 # ---------------- RUN APP ----------------
-if __name__ == "__main__":
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
     # https://flask-project-1-foev.onrender.com url
